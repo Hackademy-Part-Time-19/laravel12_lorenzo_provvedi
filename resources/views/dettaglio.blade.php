b42213b6dfc23969aa06657718950854e0c252a1
lory <lory608@gmail.com>
2024-03-09 15:13:28 +0100

    one to many

---
+++ resources/views/dettaglio.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+
+    {{-- <x-card 
+    :name="$article['name']" 
+    :titolo="$article['title']" 
+    :description="$article['description']">
+    :id="$article['id']"
+    </x-card> --}}
+    <h1>
+        asdasd
+    </h1>
+
+</x-layout>
+
+
